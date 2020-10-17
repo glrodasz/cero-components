67b551bc465f236a5519c0ba29c81879bf3e33a1
Nathaly Riaño Bejarano <31085824+NathaEEUD@users.noreply.github.com>
2020-10-17 12:00:40 -0500

    Complete the rest of the component creation script template files (#8)

--- utils/toKebabCase.js
+++ utils/toKebabCase.js
@@ -1,4 +1,8 @@
 const toKebabCase = (string) =>
-  string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+  string
+    .replace(/([A-Z])([A-Z])/g, '$1-$2')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[\s_]+/g, '-')
+    .toLowerCase()
 
 module.exports = toKebabCase
