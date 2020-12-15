52cff794c6a7b09028e966a2067bd333bdaaf2a9
Guillermo Rodas <me@guillermorodas.com>
2020-12-15 20:39:10 +0100

    ci(docs): add storybook docs for build instead

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -26,6 +26,7 @@ export const decorators = [
 export const parameters = {
   argTypes: {
     styles: { table: { disable: true } },
+    getStyles: { table: { disable: true } },
   },
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
