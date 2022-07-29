cbb21a9b34fdff42e8237eb5110acf2b0a53b815
Guillermo Rodas <me@guillermorodas.com>
2022-07-29 21:15:04 +0200

    style(jest config): sort jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
-    '\\.svg$': '<rootDir>/utils/testUtils/svgrMock.js',
-  },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
   collectCoverageFrom: [
     '{atoms,molecules,layout}/**/!(index).js',
     '{hocs,helpers}/*.js',
   ],
   coverageThreshold: {
     global: {
-      statements: 60,
       branches: 60,
-      lines: 60,
       functions: 60,
+      lines: 60,
+      statements: 60,
     },
+  },
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/utils/testUtils/svgrMock.js',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
   },
 }
