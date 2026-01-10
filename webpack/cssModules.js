25315bfa54fcaf34eccb5d0f644ada941294c993
Guillermo Rodas <glrodasz@gmail.com>
2026-01-10 22:01:21 +0100

    chore: commit changes

--- webpack/cssModules.js
+++ webpack/cssModules.js
@@ -2,37 +2,15 @@
 // yarn dev:storybook --debug-webpack
 
 const cssModules = (config) => {
-  // Find all CSS-related rules and exclude .module.css from them
-  config.module.rules.forEach((rule) => {
-    if (rule.test && rule.test.toString().includes('css')) {
-      // Exclude .module.css files from this rule
-      if (rule.exclude) {
-        // If exclude already exists, make it an array and add our exclusion
-        rule.exclude = Array.isArray(rule.exclude)
-          ? [...rule.exclude, /\.module\.css$/]
-          : [rule.exclude, /\.module\.css$/]
-      } else {
-        rule.exclude = /\.module\.css$/
-      }
-    }
+  // We search the rule defined for `*.css` files
+  const cssRule = config.module.rules.find(
+    (rule) => rule.test && rule.test.test('.css')
+  )
 
-    // Handle oneOf pattern (Storybook may use this)
-    if (rule.oneOf) {
-      rule.oneOf.forEach((oneOfRule) => {
-        if (oneOfRule.test && oneOfRule.test.toString().includes('css')) {
-          if (oneOfRule.exclude) {
-            oneOfRule.exclude = Array.isArray(oneOfRule.exclude)
-              ? [...oneOfRule.exclude, /\.module\.css$/]
-              : [oneOfRule.exclude, /\.module\.css$/]
-          } else {
-            oneOfRule.exclude = /\.module\.css$/
-          }
-        }
-      })
-    }
-  })
+  // We exclude `*.module.css` files from the previous rule
+  cssRule.exclude = /\.module\.css$/
 
-  // Add CSS Modules rule
+  // We add a our custom rule for `*.module.css` files
   config.module.rules.push({
     test: /\.module\.css$/,
     use: [
@@ -40,12 +18,7 @@ const cssModules = (config) => {
       {
         loader: 'css-loader',
         options: {
-          modules: {
-            localIdentName: '[name]__[local]--[hash:base64:5]',
-            namedExport: false,
-            exportLocalsConvention: 'as-is',
-          },
-          importLoaders: 0,
+          modules: true
         },
       },
     ],
